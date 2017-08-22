d052d7f827d0457adb5be1ab615e1ba9fc4ce23c
Tony BOTALLA <botalla@novius.fr>
2017-08-22 10:45:39 +0200

    Bugfix unknow middleware

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -4,13 +4,4 @@ namespace Novius\Backpack\Base\Http\Controllers;
 
 class AdminController extends \Backpack\Base\app\Http\Controllers\AdminController
 {
-    /**
-     * Create a new controller instance.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->middleware('adminLocalFromCookie');
-    }
 }
